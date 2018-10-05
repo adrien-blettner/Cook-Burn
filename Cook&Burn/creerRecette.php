23a7d0a3b48563536ba4c8823f3de3109ee8b332
adrien-blettner <adrien.blettner@etu.univ-amu.fr>
2018-10-05 15:23:36 +0200

    Premiere séance commit commun

---
+++ Cook&Burn/creerRecette.php
@@ -0,0 +1,11 @@
+<?php
+    require_once 'utils.inc.php';
+    start_page("creer une recette");
+?>
+<form target="traitementNouvelleRecette.php" method="POST">
+    <input id="nom" value=""/>
+    <input id="nbPersonne" value="0"
+</form>
+<?php
+    end_page();
+?>
---
+++ Cook&Burn/process_form.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: f17019047
+ * Date: 05/10/2018
+ * Time: 15:04
+ */
