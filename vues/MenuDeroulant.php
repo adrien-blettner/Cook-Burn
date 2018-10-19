3a9c37878150c306202513240eb77a453b71e2cb
d16008643 <kelyan.druez-philip@etu.univ-amu.fr>
2018-10-19 14:44:41 +0200

    zzz

--- vues/MenuDeroulant.php
+++ vues/MenuDeroulant.php
@@ -8,15 +8,16 @@ start_page ('form', array ('menu.css','MenuDeroulant.css'));
 <ul id="menu-vertical">
     <li><a href="#"></a>
         <ul>
+            <li><a></a></li>
             <li><a href="#">Profil</a></li>
-            <li><a href="#">Recettes</a></li>
-            <li><a href="#">Index</a></li>
+            <li><a href="#">Connexion</a></li>
             <li><a href="#">Déconnexion</a></li>
         </ul>
         <div class="topnav">
             <a classe="nom" href="../index.php" id="cookBurn">Cook&Burn</a>
             <a href="#recette">Recettes</a>
             <a href="#description">Description</a>
+            <a href="#"
         </div>
     </li>
 </ul>
