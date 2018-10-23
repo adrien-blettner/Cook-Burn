40355c5b9d58f36671c88be21c2027b447e7cd41
BLETTNER Adrien <b17013542@a-maq-iutinfo.salsa.univ-amu.fr>
2018-10-23 14:20:49 +0200

    Modif police + html/css vueRecette

--- vues/vueRecette.php
+++ vues/vueRecette.php
@@ -15,15 +15,15 @@ start_page ($recette->getNOM(), array ('recette.css'));
         <p class="inline"><?php echo 'Créateur: ' . $recette->getCreateur();?></p>
         <p class="inline"><?php echo 'Burns: ' . $recette->getBurn();?></p>
         <p> <?php $recette->getDescriptionLongue();?> </p>
-        <p>Ingédients:</p>
+        <p>Ingrédients:</p>
         <ol >
             <?php
             foreach ($recette->getIngredients() as $ingredient)
                 echo "\t\t\t\t". '<li>' . $ingredient . '</li>' . PHP_EOL;
             ?>
         </ol>
-        <p>Etapes:</p>
-        <ol >
+        <p>Étapes:</p>
+        <ol>
             <?php
             foreach ($recette->getEtapes() as $etapes)
                 echo "\t\t\t\t". '<li>' . $etapes . '</li>' . PHP_EOL;
