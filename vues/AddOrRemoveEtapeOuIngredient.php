1acd4a9421b6bed8497e0040e90226fae4d770e7
d16008643 <kelyan.druez-philip@etu.univ-amu.fr>
2018-10-24 17:16:51 +0200

    Ajout ou enlever étapes/ingrédients

--- vues/AddOrRemoveEtapeOuIngredient.php
+++ vues/AddOrRemoveEtapeOuIngredient.php
@@ -1,5 +1,98 @@
-<?php
 
+ <div id="dynamic_ingr">
+
+        <div id="1" class="selectingr">
+    <select id="ingr1" name="ingr1">
+        <option value="0">Ingrédient 1</option>
+        <?php
+        $BDingredient = new BD_Ingredients();
+        foreach ($BDingredient->findAll($dbLink) as $ing)
+        {
+            echo'<option value="' . $ing->getId() . '">' . $ing->getNomIngredients()  . '</option>';
+        }
+        ?>
+    </select>
+            <select class="unit" name="unit1">
+                <option value="0">Unité</option>
+                <option value="1">Masse(en gramme)</option>
+                <option value="2">Volume(en cl)</option>
+            </select>
+            <input type="text" placeholder="Quantité" name="quantite1"/>
+    <button type="button" id="1" class="btn_remove_ingr">REM</button>
+        </div>
+
+        <div id="2" class="selectingr">
+    <select id="ingr2" name="ingr2">
+        <option value="0">Ingrédient 2</option>
+        <?php
+        $BDingredient = new BD_Ingredients();
+        foreach ($BDingredient->findAll($dbLink) as $ing)
+        {
+            echo'<option value="' . $ing->getId() . '">' . $ing->getNomIngredients()  . '</option>';
+        }
+        ?>
+    </select>
+            <select class="unit" name="unit2">
+                <option value="0">Unité</option>
+                <option value="1">Masse(en gramme)</option>
+                <option value="2">Volume(en cl)</option>
+            </select>
+            <input type="text" placeholder="Quantité" name="quantite2"/>
+    <button type="button" id="2" class="btn_remove_ingr">REM</button>
+        </div>
+
+
+        <div id="3" class="selectingr">
+    <select id="ingr3" name="ingr3">
+        <option value="0">Ingrédient 3</option>
+        <?php
+        $BDingredient = new BD_Ingredients();
+        foreach ($BDingredient->findAll($dbLink) as $ing)
+        {
+            echo'<option value="' . $ing->getId() . '">' . $ing->getNomIngredients()  . '</option>';
+        }
+        ?>
+    </select>
+            <select class="unit" name="unit3">
+                <option value="0">Unité</option>
+                <option value="1">Masse(en gramme)</option>
+                <option value="2">Volume(en cl)</option>
+            </select>
+            <input type="text" placeholder="Quantité" name="quantite3"/>
+    <button type="button" id="3" class="btn_remove_ingr">REM</button>
+        </div>
+    </div>
+    <button type="button" id="add_view">ADD</button>
+
+    <div id="dynamic_etape">
+        <div id="1" class="etape">
+           <input type="text" id="text_etape1" placeholder="Etape 1" name="etape1"/>
+            <button type="button" id="1" class="btn_remove_etape">REM</button>
+        </div>
+
+        <div id="2" class="etape">
+           <input type="text" id="text_etape2" placeholder="Etape 2" name="etape2"/>
+            <button type="button" id="2" class="btn_remove_etape">REM</button>
+        </div>
+
+        <div id="3" class="etape">
+           <input type="text" id="text_etape3" placeholder="Etape 3" name="etape3"/>
+            <button type="button" id="3" class="btn_remove_etape">REM</button>
+        </div>
+
+    </div>
+    <button type="button" id="add_etape">ADD</button>
+
+    <select name="statut">
+        <option value="0">Public</option>
+        <option value="1">Privé</option>
+        <option value="2">Brouillon</option>
+    </select>
+
+    <input type="submit" name="action" value="Créer la recette"/>
+ </form>
+ <?php
+ end_page();
  ?>
 <script>
     $(document).ready(function () {
