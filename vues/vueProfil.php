c84264150e3bc4bd7adb22b571cdcf46089a081a
Joan-FABRE <joan.fabre@etu.univ-amu.fr>
2018-10-24 11:44:40 +0200

    profil

--- vues/vueProfil.php
+++ vues/vueProfil.php
@@ -3,50 +3,24 @@ require_once 'formatPage.inc.php';
 
 start_page ('Profile', array ('profil.css','rolesUtilisateur.css'));
 ?>
-    <div class="presentation">
-        <div class="presentation_groupe">
-            <table>
-                <tr>
-                    <td>
-                        <div class="presentation_membre">
-                            <p></p>
-                            <img class="image_groupe" src="images/bart.jpg" alt="Photo de "/>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="presentation_membre">
-                            <p></p>
-                            <p>Pseudo: </p>
-                            <p>Age: </p>
-                            <p>Email: </p>
-                            <label for="DateDeNaissance"><p>Date de naissance</p></label>
-                            <input min="1" max="31" class="jour" id="DateDeNaissance" placeholder="Jour" required="" type="number">
-                            <input min="1" max="12" class="mois" placeholder="Mois" required="" type="number">
-                            <input min="1900" max="2017" class="année" placeholder="Année" required="" type="number"><br><br><br>
-                            <textarea onkeyup="reste(this.value);" name="impression" rows="2" cols="50" placeholder="Ecrivez en plus sur vous ici..."></textarea>
-                            <span id="caracteres">450</span>caractères restants
-                            <script type="text/javascript">
-                                function reste(texte)
-                                {
-                                    var restants=450-texte.length;
-                                    document.getElementById('caracteres').innerHTML=restants;
-                                }
-                            </script>
-                        </div>
-                    </td>
-                </tr>
-            </table>
+
+        <div class="presentation">
+            <div class="presentation_membre">
+                <div class="titre">PROFIL</div>
+                <p></p>
+                <img class="image_groupe" src="images/bart.jpg" alt="Photo de "/>
+                <p>Pseudo: </p>
+                <p>Age: </p>
+                <p>Email: </p>
+                <label for="DateDeNaissance"><p>Date de naissance</p></label>
+                <input min="1" max="31" class="jour" id="DateDeNaissance" placeholder="Jour" required="" type="number">
+                <input min="1" max="12" class="mois" placeholder="Mois" required="" type="number">
+                <input min="1900" max="2017" class="année" placeholder="Année" required="" type="number"><br><br><br
+            </div>
         </div>
-    </div>
 
 
-    </tr>
 
-
-    <div class="bord">
-        <a href="form.php" class="bouton13">Réinitialisez votre mot de passe</a>
-        <a href="form.php" class="bouton13">Déconnexion</a>
-    </div>
 
     </head>
 
