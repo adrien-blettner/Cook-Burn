ba6be61d45f3e34c95d888a2d60d17aa38a3400e
baptistegasser <baptiste.gasser@etu.univ-amu.fr>
2018-10-25 13:53:28 +0200

    Modif liens barre nav PS:ancien commit à un msg incorrect

--- vues/vueBarreNav.php
+++ vues/vueBarreNav.php
@@ -2,13 +2,18 @@
     <ul class="sideNav">
         <li id="iconeLi"><img alt="Icone de menu déroulant" src="/vues/images/iconeMenuDeroulant.png" id="iconeImage">
             <ul class="sous-menu">
-                <li><a href="/vues/vueProfil.php">Profil</a></li>
-                <li><a href="/vues/vueConnexion.php">Connexion</a></li>
+                <li><a href="/profil">Profil</a></li>
+                <?php
+                if (!isset($_SESSION['isConnected']) or $_SESSION['isConnected'] === false)
+                    echo "\t\t\t\t" . '<li><a href="/connexion">Connexion</a></li>';
+                else
+                    echo '
                 <li>
                     <form action="/" id="deconnexionForm" method="post" >
                         <input id="deconnexionBouton" type="submit" name="action" value="Déconnexion"/>
                     </form>
-                </li>
+                </li>';
+                ?>
             </ul>
         </li>
         <li id="C&B"> <a href="/">Cook & Burn</a></li>
